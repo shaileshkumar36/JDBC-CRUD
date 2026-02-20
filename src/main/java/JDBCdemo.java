0844756f16ec1e50f2fe37c3344ad24024aaa9d3
shaileshkumar36git config --global user.email shailesraj7857@gmail.com <shaileshraj7857@gmail.com>
2026-02-20 11:00:47 +0530

    type3 connection

--- src/main/java/JDBCdemo.java
+++ src/main/java/JDBCdemo.java
@@ -7,22 +7,13 @@ public class JDBCdemo {
 
     public static void main(String args[]){
 
-        // type-2 to connect database and cloase the connection
+        // type -3 to connection the databasle and close the connection;
 
-        Connection con = null;
-        try {
-            con = DriverManager.getConnection(url, userName, password);
-            System.out.println("DB connected suscessfull");
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        } finally {
-            try {
-                con.close();
-                System.out.println("connection is close ");
-            } catch (SQLException e) {
-                throw new RuntimeException();
-            }
+        try(Connection con = DriverManager.getConnection(url, userName, password)){
+            System.out.println("database connect");
+        }catch(SQLException e){
+            e.printStackTrace();
         }
-     }
+    }
 
 }
